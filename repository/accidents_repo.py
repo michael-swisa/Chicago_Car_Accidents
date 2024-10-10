c1a0d91dc40e36e351433f294fd6bc676f05e9f8
Michael Swisa <ms0504109182@gmail.com>
2024-10-10 16:50:18 +0300

    add fanction count_accidents_by_week_and_beat

--- repository/accidents_repo.py
+++ repository/accidents_repo.py
@@ -9,3 +9,26 @@ def get_sum_accidents_by_beat(beat):
 
 
 
+def count_accidents_by_day_and_beat(beat, date):
+    count = Accidents.count_documents({
+        'LOCATION.BEAT_OF_OCCURRENCE': beat,
+        'DATES.CRASH_DATE': {'$regex': f'^{date}'}
+    })
+    return count
+
+
+def count_accidents_by_week_and_beat(beat, start_date):
+    start_date_obj = datetime.strptime(start_date, "%m/%d/%Y")
+    end_date_obj = start_date_obj + timedelta(days=7)
+    start_date_str = start_date_obj.strftime("%m/%d/%Y")
+    end_date_str = end_date_obj.strftime("%m/%d/%Y")
+    count = Accidents.count_documents({
+        'LOCATION.BEAT_OF_OCCURRENCE': beat,
+        'DATES.CRASH_DATE': {
+            '$gte': start_date_str,
+            '$lt': end_date_str
+        }
+    })
+    return count
+
+
